d94952e6ae2cab73e4080c98296f2779214a8be4
Ahmad Sami Jarir <ahmadsamijarir@gmail.com>
2025-07-01 00:11:08 +0600

    initial commit

---
+++ index.py
@@ -0,0 +1,18 @@
+print('')#space
+print('    //\\\     ||||||||                ')
+print('   //  \\\    ||                      ')
+print('  //    \\\   ||||||||                ')
+print(' //      \\\        ||                ')
+print('//        \\\ |||||||| Projects...    ')
+print('')#space
+print("All problems and solving of class ten text book. For any further info and any object cotact with AS PROJECTS. ")
+print('')#space
+print("||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||")
+print('')#space
+
+print('Here are 17 projects from Text Book. Execute them, look at the script and try at your own...')
+print('')#space
+print('')
+
+
+proj = input('Enter Selection : ')
