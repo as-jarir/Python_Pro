78b5538f39ac1179bf8693b44f83c83592555392
Ahmad Sami Jarir <ahmadsamijarir@gmail.com>
2025-07-01 00:36:28 +0600

    new num

--- index.py
+++ index.py
@@ -12,7 +12,23 @@ print('')#space
 
 print('Here are 17 projects from Text Book. Execute them, look at the script and try at your own...')
 print('')#space
+print('1. Sum of two numbers.')
 print('')
-
-
+print('')
+print('')
+print('')
+print('')
+print('')
+print('')
+print('')
+print('')
+print('')
+print('')
+print('')
+print('')
+print('')
+print('')
+print('')
+print('')
+print('')#space
 proj = input('Enter Selection : ')
