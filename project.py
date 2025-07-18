b98aade143b5eaecd4db7b2d5ca9a791171378f6
Ahmad Sami Jarir <ahmadsamijarir@gmail.com>
2025-07-18 14:55:34 +0600

    num changed and 14 opt

--- project.py
+++ project.py
@@ -5,35 +5,36 @@ print('  //    \\\   ||||||||                ')
 print(' //      \\\        ||                ')
 print('//        \\\ |||||||| Projects...    ')
 print('')#space
-print("A individual project of AS Roots.")
+print("Basic Python Problem-Solving project. A individual Project Of AS Roots.")
 print('')#space
 print("||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||")
 print('')#space
 
 print('Here are 17 projects on Python. Execute them, take a look at the script and try at your own...')
 print('')#space
-print(' 1. Sum of two numbers.')
-print(' 2. Pass-Fail identifier.')
-print(' 3. Age identifier.')
-print(' 4. Odd-Even Numbers identifier.')
-print(' 5. Leap year indentifier.')
-print(' 6. Meter to Feet and Feet to Meter converter.')
-print(' 7. Positive, negative and zero identifier.')
-print(' 8. Vowel and Consonant identifier.')
-print(' 9. Largest number identifier.')
-print('10. Multiple presentesion writer.')
-print('11. Word writer multiple time.')
-print('12. Odd-Even Number identifier between selected number.')
-print('13. 1'       )
-print('    1,2     ')
-print('    1,2,3   ')
-print("    1,2,3,4... this type of thing maker, don't know what it is :( ")
-print('14. Multiplication table writer.')
-print('15. Multiple writer using while loop.')
-print('16. 1+2+3+.....+100 ,Sum counter.')
-print('17. leaving number writer.')
+print('[ 1] Sum of two numbers.')
+print('[ 2] Pass-Fail identifier.')
+print('[ 3] Age identifier.')
+print('[ 4] Odd-Even Numbers identifier.')
+print('[ 5] Leap year indentifier.')
+print('[ 6] Meter to Feet and Feet to Meter converter.')
+print('[ 7] Positive, negative and zero identifier.')
+print('[ 8] Vowel and Consonant identifier.')
+print('[ 9] Largest number identifier.')
+print('[10] Multiple presentesion writer.')
+print('[11] Word writer multiple time.')
+print('[12] Odd-Even Number identifier between selected number.')
+print('[13] 1'       )
+print('     1,2     ')
+print('     1,2,3   ')
+print("     1,2,3,4...  ")
+print("this type of thing maker, don't know what it is :(")
+print('[14] Multiplication table writer.')
+print('[15] Multiple writer using while loop.')
+print('[16] 1+2+3+.....+100 ,Sum counter.')
+print('[17] leaving number writer.')
 print('')#space
-print('18. Show script of this code.')
+print('[18] Show script of this code.')
 print('')#space
 print('')#space
 
@@ -594,3 +595,34 @@ match proj:
         print('')#space
         chos14 = input('Insert your Option : ')
         print('')
+
+        match chos14:
+            case "1":
+                print('Multiplication table writer.')
+                print('')#Space
+
+                num = int(input('Multiplication table of which number = '))
+                print('')#space
+                tim = int(input('Up to which number = '))
+                tim += 1
+                print('')#space
+
+                for i in range(1,tim):
+                    result = num*i
+                    print(num,"*",i," = ",result)
+            case "2":
+                code14 = """
+                    print('Multiplication table writer.')
+                print('')#Space
+
+                num = int(input('Multiplication table of which number = '))
+                print('')#space
+                tim = int(input('Up to which number = '))
+                tim += 1
+                print('')#space
+
+                for i in range(1,tim):
+                    result = num*i
+                    print(num,"*",i," = ",result)
+                    """
+                print(code14)
